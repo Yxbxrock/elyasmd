a6b3a4a18184a8a16eac70bc5763faeaa2774d48
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-06-01 10:34:17 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -3,18 +3,18 @@ let handler = async (m, {conn}) => {
 const vcard = `BEGIN:VCARD
 VERSION:3.0
 N:Sy;Bot;;;
-FN: Elyas
+FN: Frmndaa
 item.ORG: frmndaa
 item1.TEL;waid=6282365089294:6282365089294@s.whatsapp.net
 item1.X-ABLabel:No Wa gw
-item2.EMAIL;type=INTERNET:😅
+item2.EMAIL;type=INTERNET:firmanman117@gmail.com
 item2.X-ABLabel:Email
 item3.ADR:;;🇮🇩INDONESIA;;;;
 item3.X-ABADR:ac
 item3.X-ABLabel:📍 LOCATION
 item4.EMAIL;type=INTERNET:Seseorang Akan Selalu Terlihat Buruk di Mata Pembenci!
 item4.X-ABLabel:somebody 👤
-item5.URL:${instagram}
+item5.URL:https://instagram.com/yxbxrock
 item5.X-ABLabel:Website
 END:VCARD`
 const sentMsg  = await conn.sendMessage(
