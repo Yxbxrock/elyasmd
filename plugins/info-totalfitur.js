ebd1ad41a5fc1326974ebc24709aa49cbf9c963b
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-06-03 18:56:47 +0700

    Create info-totalfitur.js

---
+++ plugins/info-totalfitur.js
@@ -0,0 +1,20 @@
+let handler = async (m, { conn, args, command }) => {
+let totalf = Object.values(global.plugins).filter(
+    (v) => v.help && v.tags
+  ).length;
+m.reply(conn.sendBut(m.chat, `╭━━━〔karyl-bot〕━━━⬣ 
+┃𓃠 *Version*
+┃➺ ${vs}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ *total fitur*
+┃➺ ${totalf}
+┃ *Total features currently*
+┃➺ ${totalf}
+╰━━━━━━━━━━━━━━━━━━━━━⬣`, gt , `Menu`, '.menu',  m))
+}
+
+handler.help = ['totalctr | totalfeature']
+handler.tags = ['info']
+handler.command = /^(totalfitur|totalfeature|featuretotal|ctrtotal|totalcaracteristicas|caracteristicastotal|totalctrtc|totalcttc|totalctrt|totalctrs|totalctr)$/i
+handler.exp = 5
+module.exports = handler
